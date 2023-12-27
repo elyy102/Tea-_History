0ca41c300f7414cbfa1f27e0e9e1bcce8f989ad6
mariia1604 <103173096+mariia1604@users.noreply.github.com>
2023-12-27 09:04:13 +0500

    catalog

--- src/catalog_matcha.jsx
+++ src/catalog_matcha.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import './catalog_chinatea.css'
 import { Link } from 'react-router-dom'
 import Header from './header'
 import Footer from './footer'
 import Catalog_tea from './assets/images/catalog_tea.svg'
 import Card from './assets/icons/bag.fill.svg'
+import React, {useEffect, useState } from 'react'
+import Product from './product'
 
 function Catalog_matcha() {
 
@@ -12,86 +14,16 @@ function Catalog_matcha() {
     <Header />
     <div className="wrapper">
         <div className="about_title" id='categories'>
-            <h1 className="about_h1">Матча</h1>
+            <h1 className="about_h1">Китайский чай</h1>
         </div>
-        <div className="filtering">
-            <div className="filtering_filtr">
-               <a href=""><p className="filtr">Белый</p></a>
-                <hr /> 
-            </div>
-            <div className="filtering_filtr">
-               <a href=""><p className="filtr">Зеленый</p></a>
-                <hr /> 
-            </div>
-            <div className="filtering_filtr">
-               <a href=""><p className="filtr">Красный</p></a>
-                <hr /> 
-            </div>
-            <div className="filtering_filtr">
-               <a href=""><p className="filtr">Улун</p></a>
-                <hr /> 
-            </div>
-            <div className="filtering_filtr">
-               <a href=""><p className="filtr">Пуэр</p></a>
-                <hr /> 
-            </div>
-        </div>
+        
         <div className="catalog_cards">
-            <div className="card">
-                <div className="elipse">
-                    <img src={Catalog_tea} />
-                </div>
-                <h1 className="card_title">Грация</h1>
-                <p className="composition"><span>Состав:</span> курильский чай, лист земляники, зизифора, чага, лабазник, лаванда, ягода рябины.</p>
-                <div className="card_footer">
-                    <p className="price">1 290₽</p>
-                        <img src={Card} />
-                </div>
-            </div>
-            <div className="card">
-                <div className="elipse">
-                    <img src={Catalog_tea} />
-                </div>
-                <h1 className="card_title">Грация</h1>
-                <p className="composition"><span>Состав:</span> курильский чай, лист земляники, зизифора, чага, лабазник, лаванда, ягода рябины.</p>
-                <div className="card_footer">
-                    <p className="price">1 290₽</p>
-                        <img src={Card} />
-                </div>
-            </div>
-            <div className="card">
-                <div className="elipse">
-                    <img src={Catalog_tea} />
-                </div>
-                <h1 className="card_title">Грация</h1>
-                <p className="composition"><span>Состав:</span> курильский чай, лист земляники, зизифора, чага, лабазник, лаванда, ягода рябины.</p>
-                <div className="card_footer">
-                    <p className="price">1 290₽</p>
-                        <img src={Card} />
-                </div>
-            </div>
-            <div className="card">
-                <div className="elipse">
-                    <img src={Catalog_tea} />
-                </div>
-                <h1 className="card_title">Грация</h1>
-                <p className="composition"><span>Состав:</span> курильский чай, лист земляники, зизифора, чага, лабазник, лаванда, ягода рябины.</p>
-                <div className="card_footer">
-                    <p className="price">1 290₽</p>
-                        <img src={Card} />
-                </div>
-            </div>
-            <div className="card">
-                <div className="elipse">
-                    <img src={Catalog_tea} />
-                </div>
-                <h1 className="card_title">Грация</h1>
-                <p className="composition"> <span>Состав:</span> курильский чай, лист земляники, зизифора, чага, лабазник, лаванда, ягода рябины.</p>
-                <div className="card_footer">
-                    <p className="price">1 290₽</p>
-                        <img src={Card} />
-                </div>
-            </div>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
         </div>
     </div>
     <Footer />
